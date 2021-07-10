e689745744919ed273eeb30a823ecb62f39de251
rekcuFniarB <retratserif@gmail.com>
2021-07-10 11:24:41 +0300

    New article fields (slug, available e.t.c.)

--- models.py
+++ models.py
@@ -7,8 +7,13 @@ class User(AbstractUser):
     pass
 
 class Article(MPTTModel):
-    title = models.CharField(max_length=255)
-    short = models.CharField(max_length=32)
+    title = models.CharField(max_length=255, default='')
+    ## Custom title to show in menu
+    menu_title = models.CharField(max_length=255, default='')
+    short = models.CharField(max_length=255)
+    ## Not making it unique because it may appear with same name in 
+    ## different tree level.
+    slug = models.CharField(max_length=255, default='')
     fmt = models.CharField(
         max_length = 3,
         choices = [
@@ -19,8 +24,10 @@ class Article(MPTTModel):
     )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     timestamp = models.DateTimeField(default=now)
-    show_in_menu = models.BooleanField()
-    content = models.TextField()
+    show_in_menu = models.BooleanField(default=False)
+    ## Is article available?
+    available = models.BooleanField(default=True)
+    content = models.TextField(default='')
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     
     def __str__(self):
