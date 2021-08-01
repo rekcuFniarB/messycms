fb413b581ac19b9a646d65b38c7b02f1bcbd8aab
rekcuFniarB <retratserif@gmail.com>
2021-08-01 14:15:19 +0300

    render_view template tag.

--- templatetags/messycms_tags.py
+++ templatetags/messycms_tags.py
@@ -2,6 +2,7 @@ from django import template
 from ..models import Node
 from django.utils import safestring
 from .. import plugins
+from django.urls import resolve
 
 register = template.Library()
 
@@ -24,3 +25,8 @@ def render(context, node, *args, **kwargs):
     plugins.render(node, context['request'])
     return node.content
 
+@register.simple_tag(takes_context=True)
+def render_view(context, path, *args, **kwargs):
+    resolved = resolve(path)
+    response = resolved.func(context['request'], **resolved.kwargs)
+    return safestring.mark_safe(response.content.decode(response.charset))
