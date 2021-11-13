f95dc9f2bf6f8201b6959d45161e2be9ce20c9b3
rekcuFniarB <retratserif@gmail.com>
2021-11-13 09:28:13 +0300

    JS: new playlist class.

--- src/MessyCMS/static/messycms/js/media-embed.js
+++ src/MessyCMS/static/messycms/js/media-embed.js
@@ -117,6 +117,10 @@ function MediaEmbedded(link) {
             }
             else if (frameSrc.hostname == 'w.soundcloud.com') {
                 frameSrc.searchParams.set('auto_play', 'true');
+                frameSrc.searchParams.set('show_teaser', 'false');
+                frameSrc.searchParams.set('hide_related', 'true');
+                frameSrc.searchParams.set('show_comments', 'false');
+                frameSrc.searchParams.set('show_reposts', 'false');
             }
             frameSrc.search = frameSrc.searchParams.toString();
             this.frame.src = frameSrc.href;
@@ -145,6 +149,16 @@ function MediaEmbedded(link) {
             this.frame.setAttribute('loading', 'lazy');
             this.frame.setAttribute('allowfullscreen', true);
             this.frame.setAttribute('allow', 'fullscreen; autoplay');
+            // We will resolve this promise when frame loaded
+            var promiseActions = {};
+            this.frame.ready = new Promise((resolve, reject) => {
+                promiseActions.resolve = resolve.bind(this.frame);
+                promiseActions.reject = reject.bind(this.frame);
+            });
+            Object.assign(this.frame.ready, promiseActions);
+            this.frame.addEventListener('load', function(event) {
+                event.target.ready.resolve(event);
+            });
             this.link.append(this.frame);
         } else {
             let div = document.createElement('div');
@@ -156,4 +170,152 @@ function MediaEmbedded(link) {
     }.bind(this);
     
     this.getOEmbed();
+}
+
+class MessyPlaylist {
+    current;
+    constructor(config) {
+        if (typeof config === 'object') {
+            Object.assign(this, config);
+        }
+        this[Symbol.iterator] = function() { return this; };
+        if (typeof this.container !== 'object') {
+            console.error('WARNING: no container supplied to constructor');
+        }
+        if (typeof this.list !== 'object') {
+            console.error('ERROR: no playlist element supplied to constructor');
+        } else {
+            this.list.addEventListener('click', this.onClick.bind(this));
+        }
+        this.testSound();
+        window.addEventListener('message', this.postMessagesResponse.bind(this));
+    }
+    
+    log(...args) {
+        if (!!this.debug) {
+            if (typeof console[args[0]] === 'function') {
+                return console[args[0]](...args.slice(1));
+            } else {
+                return console.info(...args);
+            }
+        }
+    }
+    
+    testSound() { // https://stackoverflow.com/a/16573282
+        // one context per document
+        var context = new (window.AudioContext || window.webkitAudioContext)();
+        var osc = context.createOscillator(); // instantiate an oscillator
+        osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
+        osc.frequency.value = 20000; // Hz
+        osc.connect(context.destination); // connect it to the destination
+        osc.start(); // start the oscillator
+        osc.stop(context.currentTime + 0.5); // stop 0.5 seconds after the current time
+    }
+    
+    postMessagesResponse(event) {
+        this.log('MESSAGE', event.data, event.origin);
+        if (!!this.current.frame && !!this.current.frame.contentWindow) {
+            if (typeof event.data === 'string') {
+                if (event.data.indexOf('"method":"ready"') > -1) {
+                    // Soundcloud ready
+                    this.current.frame.contentWindow.postMessage(JSON.stringify(
+                        {method: 'addEventListener', value: 'finish'}
+                    ), '*');
+                    this.current.frame.contentWindow.postMessage(JSON.stringify(
+                        {method: 'play'}
+                    ), '*');
+                }
+                else if (event.data.indexOf('"method":"finish"') > -1) {
+                    // SC playback finished
+                    this.play(this.next().value);
+                }
+                else if (event.data.indexOf('player:video:ended') > -1) {
+                    // 23video ended
+                    this.play(this.next().value);
+                }
+                else if (event.data == 'playerinited') {
+                    // Bandcamp.com
+                }
+            }
+        }
+    }
+    
+    onClick(event) {
+        if (event.target.classList.contains('playlist-item')) {
+            this.play(event.target, event);
+        }
+    }
+    
+    play(item, event) {
+        this.current = item;
+        if (!item) {
+            // End of playlist?
+            if (!!this.container) {
+                this.container.classList.remove('active');
+                return false;
+            }
+        }
+        var embedMedia = document.createElement('a');
+        embedMedia.classList.add('media-embed');
+        embedMedia.href = item.href;
+        Object.assign(embedMedia.dataset, item.dataset);
+        if (!!this.container) {
+            if (!!event) {
+                event.preventDefault();
+            }
+            this.container.innerHTML = '';
+            this.container.append(embedMedia);
+            this.container.classList.add('active');
+            embedMedia.embed = new MediaEmbedded(embedMedia);
+            embedMedia.embed.ready.then((embed) => {
+                this.current.frame = embed.embedFrame().frame;
+                var frameSrc = document.createElement('a');
+                frameSrc.href = this.current.frame.src;
+                if (frameSrc.hostname.indexOf('23video.com') > -1) {
+                    // Source: https://github.com/23/GlueFrame
+                    this.current.frame.ready.then(event => {
+                        this.current.frame.contentWindow.postMessage(JSON.stringify({
+                            f: 'set',
+                            cbId: 'playing',
+                            args: ['playing', true]
+                        }), '*');
+                        // Subscribing to playback end event
+                        this.current.frame.contentWindow.postMessage(JSON.stringify({
+                            f: 'bind',
+                            cbId: 'ended',
+                            args: ['player:video:ended']
+                        }), '*');
+                    });
+                }
+            });
+        }
+    }
+    
+    next() {
+        var items = [...this.list.querySelectorAll('.playlist-item')];
+        var result = {done: true, value: this.current};
+        if (items.length > 0) {
+            if (!this.current) {
+                // Beginning
+                this.current = result.value = items[0];
+                result.done = false;
+            } else {
+                var curIndex = items.indexOf(this.current) + 1;
+                if (curIndex < items.length) {
+                    // Can get next next
+                    this.current = result.value = items[curIndex];
+                    result.done = false;
+                } else {
+                    // Reached the end
+                    result.done = true;
+                    this.current = result.value = null;
+                }
+                result.curIndex = curIndex;
+            }
+        } else {
+            result.done = true;
+            this.current = result.value = null;
+        }
+        return result;
+    }
 }
