a82dc61e7cd012df0bf37cd074260d44c02c6725
Pyroman <pyroman@ofthe.top>
2024-05-07 22:17:41 +0300

    Async support for data getter for handlebars plugin.

--- src/MessyCMS/static/messycms/js/html-mess/html-mess.handlebars.plugin.js
+++ src/MessyCMS/static/messycms/js/html-mess/html-mess.handlebars.plugin.js
@@ -35,30 +35,39 @@ class HtmlMessHandlebarsPlugin {
                 }
             }
             
-            let html = template._hb_tpl_render(data);
-            let tpl = document.createElement('div');
-            tpl.innerHTML = template.outerHTML;
-            // Getting copy of template
-            tpl = tpl.children[0];
-            tpl.id = '';
-            delete tpl.dataset.select;
-            delete tpl.dataset.handlebars;
-            tpl.innerHTML = html;
-            
-            let results = [];
-            // Processing all other rules if defined
-            for (let e of this.parent.constructor.getElementsToProcess(tpl)) {
-                let r = this.parent.processTemplate(e, tpl);
-                results.push(...r);
+            let promise = data;
+            if (typeof promise.then !== 'function') {
+                promise = new Promise((resolve, reject) => {
+                    resolve(data);
+                });
             }
             
-            return Promise.all(results)
-                .then(results => {
-                    if (results.some(x => x)) {
-                        return tpl;
-                    }
-                    return false;
-                });
+            return promise.then(data => {
+                let html = template._hb_tpl_render(data);
+                let tpl = document.createElement('div');
+                tpl.innerHTML = template.outerHTML;
+                // Getting copy of template
+                tpl = tpl.children[0];
+                tpl.id = '';
+                delete tpl.dataset.select;
+                delete tpl.dataset.handlebars;
+                tpl.innerHTML = html;
+                
+                let results = [];
+                // Processing all other rules if defined
+                for (let e of this.parent.constructor.getElementsToProcess(tpl)) {
+                    let r = this.parent.processTemplate(e, tpl);
+                    results.push(...r);
+                }
+                
+                return Promise.all(results)
+                    .then(results => {
+                        if (results.some(x => x)) {
+                            return tpl;
+                        }
+                        return false;
+                    });
+            });
         }
         else {
             console.error('ERROR: Handlebars init failed.');
